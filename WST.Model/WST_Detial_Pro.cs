d31afa38feec934f30ac87e4990990bcbcba0c39
sxy-zzj <79304949+sxy-zzj@users.noreply.github.com>
2021-02-24 14:25:28 +0800

    栏目滚动图片表&&栏目内容表

---
+++ WST.Model/WST_Detial_Pro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WST.Model
+{
+    //栏目内容表
+    public class WST_Detial_Pro
+    {
+        public int Did { get; set; }          //  栏目内容表主键
+        public string Dtitla { get; set; }    //  栏目内容名称
+        public string UrlKey { get; set; }    //  UrlKey
+        public bool Dstate { get; set; }      //  内容状态
+        public string Dtype { get; set; }     //  内荣类型（）
+        public string Daddress { get; set; }  //  地址
+        public string Dexplain { get; set; }  //  说明
+        public int Dsort { get; set; }        //  排序
+    }
+}
---
+++ WST.Model/WST_Scroll_Picture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WST.Model
+{
+    // 栏目滚动图片表
+    public class WST_Scroll_Picture
+    {
+        public int Sid { get; set; }               // 滚动图片主键
+        public string Sprograma { get; set; }      // 栏目
+        public string Stitle { get; set; }         // 图片标题
+        public string Scontent { get; set; }       // 图片内容
+        public bool Sstate { get; set; }           // 图片状态
+        public string Simg { get; set; }           // 图片
+        public string Slink { get; set; }          // 图片链接
+        public DateTime Stime { get; set; }        // 图片更新的时间
+    }
+}
