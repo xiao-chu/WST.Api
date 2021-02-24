8cb70ce14b6a3c12746a9a0fab4466093b6c8674
修龙 储 <cxl13399642800@outlook.com>
2021-02-24 14:58:01 +0800

    创建表地区，黑名单，投诉

---
+++ WST.Model/WST_Area.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WST.Model
+{
+    public class WST_Area
+    {
+        public int Aid		 { get; set; }  //主键
+        public string Aname	 { get; set; }  //地址名称
+        public int Apid { get; set; }       //父级id
+    }
+}
---
+++ WST.Model/WST_Complaint.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WST.Model
+{
+    public class WST_Complaint
+    {
+        public int Cid { get; set; }                      //序号
+        public string Cnumber { get; set; }               //投诉编号
+        public bool Ctype { get; set; }                   //投诉类别  1：实名  0：匿名
+        public string Cinformation { get; set; }          //身份信息
+        public string Cbyname { get; set; }               //被投诉人姓名
+        public string Cphone { get; set; }                //电话
+        public string Cweixin { get; set; }               //微信
+        public string Ceamil { get; set; }                //邮箱
+        public string Cip { get; set; }                   //投诉人IP
+        public bool Cisfujian { get; set; }               //是否有附件
+        public bool Cstate { get; set; }                  //状态
+        public DateTime Ctime { get; set; }               //投诉时间
+        public string Cpeople { get; set; }               //处理人
+        public string Cname { get; set; }                 //投诉人姓名
+        public string Cfujian { get; set; }               //附件
+        
+    }
+}
